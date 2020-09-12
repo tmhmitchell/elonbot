6e8a70ae8e4d956d4a18a5e96feb6cd202ba44ca
aphe-lion <tom.0505@hotmail.co.uk>
2020-09-12 17:33:01 +0100

    elon: add stonks command for checking arbitrary stock prices

--- elon.py
+++ elon.py
@@ -9,24 +9,17 @@ BOOT_TIME = dt.datetime.now(tz=dt.timezone.utc)
 BOT_TOKEN = os.environ.get('ELONBOT_TG_TOKEN')
 FINNHUB_TOKEN = os.environ.get('ELONBOT_FH_TOKEN')
 
-updater = ext.Updater(
-    token=BOT_TOKEN, 
-    use_context=True
-)
-dispatcher = updater.dispatcher
 
-
-def get_current_tsla_price():
+def get_current_price(symbol):
     res = requests.get(
         'https://finnhub.io/api/v1/quote',
         params={
-            'symbol': 'TSLA',
+            'symbol': symbol,
             'token': FINNHUB_TOKEN 
         }
     )
     return res.json().get('c')
-
-
+   
 def tsla_price_handler(update, context):
     # Ignore any message that came through prior to the bot booting
     if BOOT_TIME > update.message.date:
@@ -39,19 +32,37 @@ def tsla_price_handler(update, context):
 
     message_text = update.message.text
 
-    if any(regex.search(message_text) for regex in regexes):
-        price = get_current_tsla_price()
+    if any(regex.search(update.message.text) for regex in regexes):
+        price = get_current_price('TSLA')
         context.bot.send_message(
             chat_id=update.effective_chat.id,
             text=f'TSLA @ ${price}'
         )
 
-# Add all our command handlers
-dispatcher.add_handler(
-    ext.MessageHandler(
-        ext.Filters.text & (~ext.Filters.command), 
-        tsla_price_handler
-    )
-)
+def stonks_command_handler(update, context):
+    if BOOT_TIME > update.message.date:
+        return
+    
+    if len(context.args) != 1:
+        return
 
-updater.start_polling()
+    # First argument to command is a stock symbol
+    price = get_current_price(context.args[0].upper())
+    update.message.reply_text(f'${price}')
+
+if __name__ == '__main__':
+    updater = ext.Updater(
+        token=BOT_TOKEN, 
+        use_context=True
+    )
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(
+        ext.MessageHandler(
+            ext.Filters.text & (~ext.Filters.command),
+            tsla_price_handler
+        )
+    )
+    dispatcher.add_handler(ext.CommandHandler('stonks', stonks_command_handler))
+
+    updater.start_polling()
